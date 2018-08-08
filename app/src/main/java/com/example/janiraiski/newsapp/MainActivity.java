99458ec64af7a8fe89bdb52028a9d5d424cbfe04
Jani <jani.raiski@gmail.com>
2018-08-08 20:32:37 +0300

    Added settings activity and tweaked the way system gets information from internet.

--- app/src/main/java/com/example/janiraiski/newsapp/MainActivity.java
+++ app/src/main/java/com/example/janiraiski/newsapp/MainActivity.java
@@ -6,12 +6,16 @@ import android.content.Context;
 import android.content.Loader;
 import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -25,9 +29,11 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int ARTICLE_LOADER_ID = 1;
 
+    private String apiKey = "19628701-792e-4ffc-8052-a6676111ff57";
+
     // URL to query The Guardian articles
     private static final String GUARDIAN_REQUEST_URL =
-            "https://content.guardianapis.com/search?section=technology&show-tags=contributor&api-key=19628701-792e-4ffc-8052-a6676111ff57";
+            "https://content.guardianapis.com/search?";
 
     private ArticleAdapter mAdapter;
     private TextView mEmptyStateTextView;
@@ -82,7 +88,30 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
     @Override
     public Loader<List<Article>> onCreateLoader(int i, Bundle bundle) {
-        return new ArticleLoader(this, GUARDIAN_REQUEST_URL);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String numberOfArticles = sharedPrefs.getString(
+                getString(R.string.settings_number_of_articles_key),
+                getString(R.string.settings_number_of_articles_default)
+        );
+
+        String selectedSection = sharedPrefs.getString(
+                getString(R.string.settings_section_key),
+                getString(R.string.settings_section_default)
+        );
+
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("section", selectedSection);
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("page-size", numberOfArticles);
+        uriBuilder.appendQueryParameter("api-key", apiKey);
+
+
+        return new ArticleLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -100,4 +129,20 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         mAdapter.clear();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
